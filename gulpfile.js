6cbc6b784848d58373995f07fcde40ec909f05e0
John Brennan <john@jhnbrnn.com>
2016-02-27 23:29:24 -0500

    Add build process: Gulp/Webpack
    - I know Webpack can handle CSS and I'm pretty sure it's got something
      for SASS compilation, but I don't want to get too far down the rabbit
    hole at the moment. I know Gulp works, so that's what I'm going with
    right now.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass({
+      includePaths: require('node-neat').includePaths
+    }))
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
